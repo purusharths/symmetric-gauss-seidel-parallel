1134eb466e46d0cc29eb4c8fc5d6e07c959f7cac
Purusharth Saxena <purusharth.saxena1@gmail.com>
2021-07-26 17:51:19 +0200

    update loop ordering in k-mean loop

--- k-mean-stencil/stencil_vanilla.hh
+++ k-mean-stencil/stencil_vanilla.hh
@@ -8,14 +8,25 @@ void stencil_mean_vanilla(int n, int k, double *grid, double *local_mean) {
     for (int j = 0; j < n; j++) {
       double sum = 0;
       double points = 0; // max_points;
-      for (int mm = std::max(j - k, 0); mm <= std::min(j + k, n - 1); mm++) {
-        for (int ll = std::max(i - k, 0); ll <= std::min(i + k, n - 1); ll++) {
+      int col_max = std::min(j + k, n - 1);
+      int col_min = std::max(j - k, 0);
+      int row_max = std::min(i + k, n - 1);
+      int row_min = std::max(i - k, 0);
+      int m = col_max - col_min;
+      int o = row_max - row_min;
+      int mn = (m + 1) * (o + 1);
+      // for (int mm = std::max(j - k, 0); mm <= std::min(j + k, n - 1); mm++) {
+      // for (int ll = std::max(i - k, 0); ll <= std::min(i + k, n - 1); ll++) {
+
+      for (int mm = row_min; mm <= row_max; mm++) {
+        for (int ll = col_min; ll <= col_max; ll++) {
           sum += grid[ll * n + mm];
-          points += 1;
+          // points += 1;
         }
         // local_mean[j * n + i] = sum; previous location
       }
-      local_mean[j * n + i] = static_cast<double>(sum / points); // / change to multiply
+      local_mean[j * n + i] =
+          static_cast<double>(sum / mn); // / change to multiply
     }
   }
 }
